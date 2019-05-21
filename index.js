4c84f31b471d98aea3a71405cf6d905af4c8d267
Muhamad Adam 84 <muhamadadam20@gmail.com>
2019-05-21 21:08:57 +0700

    Project Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const app = require("express")();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.get("/students", (req, res) => {
+
+    res.end("MENAMPILKAN SISWA");
+
+})
+
+app.get("/students/:nama", (req, res) => {
+
+    let namaSiswa = req.params.nama;
+
+    res.end("Menampilkan data siswa dengan nama: " + namaSiswa);
+    
+})
+
+app.post("/students", (req, res) => {
+
+    let namaSiswa = req.body.nama;
+    let alamat = req.body.alamat;
+
+    res.end("MENAMBAH DATA SISWA DENGAN NAMA: "+ namaSiswa + " ALAMAT: "+ alamat);
+
+})
+
+app.put("/students/:id", (req, res) => {
+
+    let id = req.params.id;
+    let namaSiswa = req.body.nama;
+    let alamat = req.body.alamat;
+
+    res.end("MENGUPDATE DATA SISWA DENGAN ID: " + id + " MENJADI NAMA: " + namaSiswa + " ALAMAT: " + alamat);
+})
+
+app.delete("/students/:id", (req, res) => {
+
+    let id = req.params.id;
+
+    res.end("MENGHAPUS DATA SISWA DENGAN ID: " + id);
+
+})
+
+app.listen(3000, (err) => {
+
+    if(err) throw err;
+    console.log("SERVER BERHASIL DIMULAI");    
+
+})
